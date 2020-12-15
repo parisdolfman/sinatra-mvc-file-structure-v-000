0019dba2e7db72ec3d5d0d10b41f8caa13994d3c
parisdolfman <parisdolfman@gmail.com>
2020-12-15 15:54:25 -0500

    commit tree b31406ad81cb9f8606890e58ca6aac51fdcda65c

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,18 @@
-class Model
+class Dog
+
+    attr_accessor :name, :breed, :age
+
+    @@all = []
+
+    def initialize(name, breed, age)
+      @name = name
+      @age = age
+      @breed = breed 
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end 
 
 end
